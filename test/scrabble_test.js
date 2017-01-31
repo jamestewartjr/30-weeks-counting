23985e8475c9c92c17e740f5d70f4991e429e611
James D Stewart Jr <jamstewjr@gmail.com>
2017-01-31 11:31:59 -0800

    Setup scrabble bag test.

---
+++ test/scrabble_test.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect
+const scrabble = require('./scripts/scrabble.js')
+
+
+
+describe( "Scrabble", () => {
+
+  //If user gives input of letters playe
+  it( "getInput() will receive used letters.", () => {
+    expect( getInput() ).to.be.instanceof(String)
+  })
+  //Output the number and count left in the bag.
+
+})
